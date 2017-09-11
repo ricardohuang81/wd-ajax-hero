3aaecb08b693c70740f6a63c8b6123386da31677
ricardohuang81 <huangricky@gmail.com>
2017-09-10 22:49:32 -0400

    Initial commit ajax-hero

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 
   const movies = [];
 
+  //var movies = [];
+
   const renderMovies = function() {
     $('#listings').empty();
 
@@ -57,4 +59,61 @@
   };
 
   // ADD YOUR CODE HERE
+
+  var searchForm = document.querySelector('form');
+  searchForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    var searchTerm = document.querySelector('#search').value;
+    if (searchTerm != ""){
+      //document.querySelector('#search').value = "";
+      $.get('https://omdb-api.now.sh/?s=' + searchTerm, function(data){
+
+        movies.length = 0;
+        for(var i = 0; i < data.Search.length; i++){
+          var movie = {};
+          movie.title = data.Search[i].Title;
+          movie.poster = data.Search[i].Poster;
+          movie.year = data.Search[i].Year;
+          movie.id = data.Search[i].imdbID;
+          movies.push(movie);
+          console.log(data.Search[i]);
+        }
+//console.log(data);
+      // if (movie.id){
+      //   $.get('https://omdb-api.now.sh/?i=' + movies[i].id, function(data){
+      //     movies.plot = data.Plot;
+      //   })
+      // }
+
+
+//console.log(data);
+// console.log(movies);
+//         for (var i = 0; i < movies.length; i++){
+//
+//           var wrapping_function = function(contextCopy){
+//             $.get('https://omdb-api.now.sh/?i=' + movies[i].id, function(data){
+//               movies[contextCopy].plot = data.Plot;
+//               console.log(data);
+//
+//               renderMovies();
+//             });
+//           };
+//           wrapping_function(i);
+//         }
+
+
+        // var template = "<div class=\"row\"><div class=\"col s12 m7\"><div class=\"card\"><div class=\"card-image\"><img src=\"$poster$\"><span class=\"card-title\">$title$</span></div><div class=\"card-content\"></div><div class=\"card-action\"><a href=\"#\">$id$</a></div></div></div></div>";
+        //
+        //
+        // for (var i = 0; i < data.Search.length; i++) {
+        //   var movie = data.Search[i];
+        //   var movie_html = template.replace("$poster$", movie.Poster);
+        //   movie_html = movie_html.replace("$title$", movie.Title);
+        //   movie_html = movie_html.replace("$id$", movie.imdbID);
+        //   $('#listings').append(movie_html);
+        // }
+      });
+    }
+  renderMovies();
+  });
 })();
